845d67ed44b8b05fc0a4db716733b79c7f768eb5
Smellyarmpit623 <ssww0623@outlook.com>
2022-04-22 18:24:11 +1000

    login

--- Main.py
+++ Main.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template  # import flask
+from flask import Flask,render_template,request,redirect,flash,session,url_for,g
+import sqlite3
+import hashlib
+from flask_bootstrap import Bootstrap
+from datetime import datetime,date
+from flask_wtf.file import FileField, FileAllowed
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,TextAreaField,PasswordField,SelectMultipleField,widgets,SelectField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired,URL,Optional,InputRequired
+import random
+
 app = Flask(__name__)             # create an application instance called app
 
 
 @app.route("/")
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/index')
 def index():
+    if 'user_name' in session:
+        return render_template('index.html')
+    return redirect(url_for('login'))
     return render_template('index.html')
 
 @app.route('/BuyNFTs')
 def BuyNFTs():
     return render_template('BuyNFTs.html')
+
+@app.route('/BuyCoin')
+def BuyCoin():
+    return render_template('BuyCoin.html')
+
+
 
 if __name__ == "__main__":
     host='127.0.0.1'
